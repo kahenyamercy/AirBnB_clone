aed50588c423e3662f3e14a3c036a47dd4c9f5f6
wamae-ndiritu <wamaejoseph392@gmail.com>
2023-10-12 16:14:38 +0000

    Define show command to show an instance based on class name and id

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ Entry point of the command interpreter
 """
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -46,6 +47,28 @@ class HBNBCommand(cmd.Cmd):
         class_instance = self.class_name_to_class[class_name]()
         class_instance.save()
         print(class_instance.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        class_name = self.search_for_class(args[0]);
+        if class_name:
+            name_and_id = f"{args[0]}.{args[1]}"
+            objects = storage.all()
+            for key, value in objects.items():
+                if key == name_and_id:
+                    print(objects[key])
+                    new_instance = self.class_name_to_class[class_name](**objects[key])
+                    print(new_instance)
 
     def search_for_class(self, name):
         for key, value in self.class_name_to_class.items():
