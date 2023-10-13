e69f4d418cdbc694e6f72a583ed59b13cc03fb7c
kahenyamercy <kahenyamercy5@gmail.com>
2023-10-13 05:56:14 +0000

    added the empty line chek to the command prompt

--- console.py
+++ console.py
@@ -1,99 +1,120 @@
 #!/usr/bin/python3
-"""Entry point of the command interpreter"""
+"""
+Entry point of the command interpreter
+"""
 import cmd
-from models import storage
 from models.base_model import BaseModel
-
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
-    """Command prompt"""
+    """
+    Command Interpreter for the AirBnB_clone
+    """
     prompt = '(hbnb) '
-    valid_classes = ["BaseModel"]
+    class_name_to_class = {
+            "BaseModel": BaseModel
+            }
 
-    def do_create(self, arg):
-        """Create a new instance of BaseModel and save it to the JSON file"""
-        if not arg:
-            print("** class name missing **")
-        elif arg not in self.valid_classes:
-            print("** class doesn't exist **")
-        else:
-            obj = BaseModel()
-            obj.save()
-            print(obj.id)
+    def do_quit(self, line):
+        """Quit command to exit the program
 
-    def do_show(self, arg):
-        """Prints the string representation of an instance"""
-        args = arg.split()
-        if not arg:
-            print("** class name missing **")
-        elif args[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance id missing **")
-        else:
-            key = args[0] + '.' + args[1]
-            objects = storage.all()
-            if key not in objects:
-                print("** no instance found **")
-            else:
-                print(objects[key])
-
-    def do_destroy(self, arg):
-        """Deletes an instance based on the class name and id"""
-        args = arg.split()
-        if not arg:
-            print("** class name missing **")
-        elif args[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance id missing **")
-        else:
-            key = args[0] + '.' + args[1]
-            objects = storage.all()
-            if key not in objects:
-                print("** no instance found **")
-            else:
-                del objects[key]
-                storage.save()
-
-    def do_all(self, arg):
-        """Prints all string representations of instances"""
-        objects = storage.all()
-        if not arg:
-            print([str(objects[obj]) for obj in objects])
-        elif arg not in self.valid_classes:
-            print("** class doesn't exist **")
-
-    def do_update(self, arg):
-        """Updates an instance based on the class name and id"""
-        args = arg.split()
-        if not arg:
-            print("** class name missing **")
-        elif args[0] not in self.valid_classes:
-            print("** class doesn't exist **")
-        elif len(args) < 2:
-            print("** instance id missing **")
-        elif len(args) < 3:
-            print("** attribute name missing **")
-        elif len(args) < 4:
-            print("** value missing **")
-        else:
-            key = args[0] + '.' + args[1]
-            objects = storage.all()
-            if key not in objects:
-                print("** no instance found **")
-            else:
-                setattr(objects[key], args[2], args[3][1:-1])
-                objects[key].save()
+        """
+        return True
 
     def emptyline(self):
         """Do nothing when an empty line is entered"""
         pass
 
     def do_EOF(self, line):
-        """Exit the program when EOF is encountered"""
+        """
+        Handle end of file
+        """
         print()
         return True
+
+    def do_create(self, arg):
+        """
+        Creates a new instance of class,
+        saves it (to the JSON file) and prints the id
+
+        Returns:
+            On successful instantiation, it returns
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        class_name = self.search_for_class(args[0])
+        if not class_name:
+            return
+        class_instance = self.class_name_to_class[class_name]()
+        class_instance.save()
+        print(class_instance.id)
+
+    def do_show(self, arg):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        class_name = self.search_for_class(args[0]);
+        if class_name:
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+
+            name_and_id = f"{args[0]}.{args[1]}"
+            objects = storage.all()
+
+            for key, value in objects.items():
+                if key == name_and_id:
+                    new_instance = self.class_name_to_class[class_name](**objects[key])
+                    print(new_instance)
+                    return
+            print("** no instance found **")
+
+    def do_destroy(self, arg):
+        """
+        Deletes an insatnce based on the class name and id
+        and save the changes into the JSON file
+        """
+        if not arg:
+            print("** class name missing **")
+            return
+        args = arg.split()
+        class_name = self.search_for_class(args[0]);
+        if class_name:
+            if class_name:
+                if len(args) < 2:
+                    print("** instance id missing **")
+                    return
+
+            name_and_id = f"{args[0]}.{args[1]}"
+            objects = storage.all()
+            filtered_objects = {}
+            instance_found = False
+
+            for key, value in objects.items():
+                if key == name_and_id:
+                    instance_found = True
+                else:
+                    filtered_objects[key] = value
+            if not instance_found:
+                print("** no instance found **")
+                print(objects)
+                print(filtered_objects)
+            storage.objects = filtered_objects # replace the __objects with the filtered to effect changes when saved
+            storage.save()
+
+    def search_for_class(self, name):
+        for key, value in self.class_name_to_class.items():
+            if name == key:
+                return key
+            print("** class doesn't exist **")
+        return
 
 
 if __name__ == '__main__':
